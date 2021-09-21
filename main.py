ce20224756ae7118de6df5be6364e7d43464fa2b
oliverhilstrom <819492@gmail.com>
2021-09-21 14:57:30 -0500

    8

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e):
-    return a + " " + b + " " + c + " " + d + " " + e + " "
+    return a + " " + b + " " + c + " " + d + " " + e
 print(topFiveMovies("Scott Pilgrim.", "Cabin in the Woods.", "Fantastic Mr. Fox.", "The Life Aquatic.", "Grand Budapest Hotel."))
